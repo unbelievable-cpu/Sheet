503d09f6ec96af98523744e849b0b7139740ca8e
unbelievable-cpu <59386338+unbelievable-cpu@users.noreply.github.com>
2024-12-24 11:16:25 +0200

    change.js

--- change.js
+++ change.js
@@ -1,7 +1,35 @@
 window.onload = () => {
-  let shapeSelect = document.querySelector('.select-one[data-name="shape"]');
+  const shapeSelect = document.querySelector('.select-one[data-name="shape"]');
   if (shapeSelect) {
-  shapeSelect.value = "512x768"; // Встановлюємо потрібне значення
-  shapeSelect.dispatchEvent(new Event("change")); // Емуляція вибору
+    // Установлюємо дефолтне значення, якщо його немає
+    if (!shapeSelect.value) {
+      shapeSelect.value = "512x768"; // Значення за замовчуванням
+    }
+    // Викликаємо подію, щоб система зрозуміла, яке значення обране
+    shapeSelect.dispatchEvent(new Event("change"));
+  }
+};
+
+window.onload = () => {
+  let numImages = document.querySelector('.select-one[data-name="numImages"]');
+  if (numImages) {
+    
+    if (!numImages.value) {
+      numImages.value = "6"; 
+    }
+    
+    numImages.dispatchEvent(new Event("change"));
+  }
+};
+
+window.onload = () => {
+  let category = document.querySelector('.select-one[data-name="category"]');
+  if (category) {
+    
+    if (!category.value) {
+      category.value = ",";
+    }
+    
+    category.dispatchEvent(new Event("change"));
   }
 };
